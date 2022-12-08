80ef4d8431a993aac18c4bbab4939c25d33ac47e
Henrique Barbosa <henrikleod@gmail.com>
2022-12-08 09:13:47 -0300

    feat: button component

---
+++ src/components/atomics/buttons/Button/index.tsx
@@ -0,0 +1,97 @@
+import React, { ButtonHTMLAttributes } from "react";
+import { ReactComponent as RibonIcon } from "assets/icons/ribon.svg";
+import theme from "styles/theme";
+import * as S from "./styles";
+
+export type onClickType = () => void;
+
+export type Props = {
+  text: string;
+  textColor?: string;
+  backgroundColor?: string;
+  softDisabled?: boolean;
+  borderColor?: string;
+  ribons?: boolean;
+  ribonsColor?: string;
+  leftIcon?: string;
+  rightIcon?: string;
+  altLeftIconText?: string;
+  altRightIconText?: string;
+  onClick: onClickType;
+  outline?: boolean;
+  disabled?: boolean;
+  round?: boolean;
+  size?: string;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export default function Button({
+  text,
+  textColor,
+  backgroundColor = theme.colors.green30,
+  softDisabled = false,
+  borderColor = "",
+  ribons = false,
+  ribonsColor = theme.colors.green30,
+  leftIcon,
+  rightIcon,
+  onClick,
+  altLeftIconText = "left icon",
+  altRightIconText = "right icon",
+  outline = false,
+  disabled = false,
+  round = false,
+  size = "large",
+
+  ...props
+}: Props): JSX.Element {
+  function activeTextColor() {
+    if (outline && !textColor) return theme.colors.green30;
+    if (softDisabled) return theme.colors.gray30;
+    if (!textColor) return theme.colors.neutral10;
+
+    return textColor;
+  }
+
+  function activeBackgroundColor() {
+    if (outline || softDisabled) return theme.colors.neutral10;
+    if (disabled) return theme.colors.gray30;
+
+    return backgroundColor;
+  }
+
+  function activeBorderColor() {
+    if (outline && !borderColor) return theme.colors.green30;
+    if (disabled && !borderColor) return theme.colors.gray30;
+    if (softDisabled) return theme.colors.gray20;
+
+    return borderColor;
+  }
+
+  function borderRadius() {
+    if (round) return "80px";
+
+    return "8px";
+  }
+
+  return (
+    <S.Container
+      textColor={activeTextColor()}
+      backgroundColor={activeBackgroundColor()}
+      borderColor={activeBorderColor()}
+      ribonsColor={ribonsColor}
+      onClick={onClick}
+      leftIcon={leftIcon}
+      disabled={disabled}
+      borderRadius={borderRadius()}
+      size={size}
+      {...props}
+    >
+      {leftIcon && <img id="left-icon" src={leftIcon} alt={altLeftIconText} />}
+      {text}
+      {rightIcon && (
+        <img id="right-icon" src={rightIcon} alt={altRightIconText} />
+      )}
+      {ribons && <RibonIcon />}
+    </S.Container>
+  );
+}
---
+++ src/components/atomics/buttons/Button/styles.ts
@@ -0,0 +1,71 @@
+import styled, { css } from "styled-components";
+import {
+  defaultComponentTextLarge,
+  defaultComponentTextMedium,
+  defaultComponentTextSmall,
+} from "styles/typography/default";
+
+export const Container = styled.button<{
+  textColor?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  ribonsColor?: string;
+  leftIcon?: string;
+  disabled?: boolean;
+  borderRadius?: string;
+  size?: string;
+}>`
+  ${(props) =>
+    props.size === "large" &&
+    css`
+      ${defaultComponentTextLarge}
+
+      padding: 12px 16px;
+    `}
+
+  ${(props) =>
+    props.size === "medium" &&
+    css`
+      ${defaultComponentTextMedium}
+
+      padding: 12px 16px;
+    `}
+    
+  
+  ${(props) =>
+    props.size === "small" &&
+    css`
+      ${defaultComponentTextSmall}
+
+      padding: 6px 16px;
+    `}
+    
+  width: 100%;
+  border: 1px solid #000;
+  border-color: ${(props) => props.borderColor || props.backgroundColor};
+  border-radius: ${(props) => props.borderRadius || "8px"};
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.backgroundColor};
+  color: ${(props) => props.textColor};
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
+  cursor: pointer;
+
+  #left-icon {
+    margin-right: 4px;
+  }
+
+  #right-icon {
+    margin-left: 4px;
+  }
+
+  svg {
+    margin-left: 2px;
+
+    path {
+      fill: ${(props) => props.ribonsColor};
+    }
+  }
+`;
