f8f1ab69193c3b8d9df2fbce314f6c0e9eb7e358
Nicholas Marques <nnmarques97@gmail.com>
2022-12-13 13:18:33 -0300

    feat: add shared and donation related statistics

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClientComponent } from "@ribon/shared/dist/hooks";
 import RoutesComponent from "config/routes";
 import GlobalStyle from "./styles/globalStyle";
 import ModalProvider from "./contexts/modalContext";
 import theme from "./styles/theme";
 
 function App() {
-  const queryClient = new QueryClient();
-
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientComponent>
       <ThemeProvider theme={theme}>
         <ModalProvider>
           <GlobalStyle />
@@ -19,7 +17,7 @@ function App() {
           </BrowserRouter>
         </ModalProvider>
       </ThemeProvider>
-    </QueryClientProvider>
+    </QueryClientComponent>
   );
 }
 
--- src/pages/CommunityParticipationPage/index.tsx
+++ src/pages/CommunityParticipationPage/index.tsx
@@ -1,21 +1,28 @@
 import IconAndText from "components/atomics/IconAndText";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RibonIconSquared from "assets/icons/ribon-icon-squared.svg";
 import RangeDatePicker from "components/moleculars/RangeDatePicker";
 import { useTranslation } from "react-i18next";
+import { useIntegrationImpact } from "@ribon/shared/dist/hooks";
 import * as S from "./styles";
 
 function CommunityParticipationPage(): JSX.Element {
   const { t } = useTranslation("translation", {
     keyPrefix: "communityParticipationPage",
   });
-
   const [endDate, setEndDate] = useState<Date>(new Date());
 
   const previousDate = (date: Date, days: number) =>
     new Date(new Date(date).setDate(new Date(date).getDate() - days));
 
   const [startDate, setStartDate] = useState<Date>(previousDate(endDate, 7));
+  const { integrationImpact } = useIntegrationImpact(
+    new URL(window.location.href).searchParams.get("integration_id"),
+  );
+
+  useEffect(() => {
+    console.log(integrationImpact);
+  }, [integrationImpact]);
 
   return (
     <S.Container>
@@ -30,6 +37,26 @@ function CommunityParticipationPage(): JSX.Element {
           handleStartDateChange={setStartDate}
           handleEndDateChange={setEndDate}
         />
+
+        <S.ContentContainer>
+          <S.ContentDiv>
+            <S.ParticipatingDonorsText>
+              1.286{integrationImpact?.totalDonors}
+            </S.ParticipatingDonorsText>
+            <S.ParticipatingDonorsSubtext>
+              {t("participatingDonors")}
+            </S.ParticipatingDonorsSubtext>
+          </S.ContentDiv>
+
+          <S.ContentDiv>
+            <S.ParticipatingDonorsText>
+              48.499{integrationImpact?.totalDonations}
+            </S.ParticipatingDonorsText>
+            <S.ParticipatingDonorsSubtext>
+              {t("donationsMade")}
+            </S.ParticipatingDonorsSubtext>
+          </S.ContentDiv>
+        </S.ContentContainer>
       </S.InnerContainer>
     </S.Container>
   );
