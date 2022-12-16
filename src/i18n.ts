72792fe9d08839c5bf3783ff919504ff43e90890
Nicholas Marques <nnmarques97@gmail.com>
2022-12-16 09:31:52 -0300

    feat: add i18next browser language detector

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,16 +1,18 @@
 import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
     debug: false,
     ns: "translation",
     defaultNS: "translation",
-    preload: ["en"],
+    preload: ["en", "pt"],
     cleanCode: true,
     interpolation: {
       escapeValue: false,
