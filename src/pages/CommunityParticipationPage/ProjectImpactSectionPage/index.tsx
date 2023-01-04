fe925f8c65b42b867de7c6b4953c08d4cc58aa93
Henrique Barbosa <henrikleod@gmail.com>
2023-01-04 11:57:48 -0300

    feat: statistic filtering

--- src/pages/CommunityParticipationPage/ProjectImpactSectionPage/index.tsx
+++ src/pages/CommunityParticipationPage/ProjectImpactSectionPage/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import CardCroppedImage from "components/moleculars/cards/CardCroppedImage";
 import GroupButtons from "components/sections/GroupButtons";
 import ImpactOn from "assets/icons/check-spark-dark-green.svg";
@@ -17,6 +18,8 @@ function ProjectImpactSectionPage({ integrationImpact }: Props): JSX.Element {
   const { t } = useTranslation("translation", {
     keyPrefix: "impactPerProjectSection",
   });
+
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(0);
 
   const filterOptions = [
     {
@@ -39,26 +42,59 @@ function ProjectImpactSectionPage({ integrationImpact }: Props): JSX.Element {
     },
   ];
 
+  const handleButtonChange = (element: any, index: number) => {
+    setSelectedButtonIndex(index);
+  };
+
+  const currentTabData = () => {
+    switch (selectedButtonIndex) {
+      case 0:
+        return {
+          object: integrationImpact?.impactPerNonProfit,
+          currentItem: (item: any) => ({
+            description: item?.nonProfit?.impactDescription,
+            value: item?.impact || 0,
+          }),
+        };
+      case 1:
+        return {
+          object: integrationImpact?.donorsPerNonProfit,
+          currentItem: (item: any) => ({
+            description: t("participatingDonors"),
+            value: item?.donors || 0,
+          }),
+        };
+      default:
+        return {
+          object: integrationImpact?.donationsPerNonProfit,
+          currentItem: (item: any) => ({
+            description: t("donationsMade"),
+            value: item?.donations || 0,
+          }),
+        };
+    }
+  };
+
   return (
     <S.Container>
       <S.Title>{t("title")}</S.Title>
       <GroupButtons
         elements={filterOptions}
+        onChange={handleButtonChange}
+        indexSelected={selectedButtonIndex}
         nameExtractor={(element) => element.text}
       />
       <S.ImpactContainer>
-        {integrationImpact?.impactPerNonProfit.map(
-          ({ impact, nonProfit }: any) => (
-            <div key={nonProfit.id}>
-              <CardCroppedImage
-                image={nonProfit.backgroundImage}
-                secondaryText={nonProfit.impactDescription}
-                mainText={impact}
-                internalImage={nonProfit.logo}
-              />
-            </div>
-          ),
-        )}
+        {currentTabData()?.object.map((item: any) => (
+          <div key={item.nonProfit.id}>
+            <CardCroppedImage
+              image={item.nonProfit.backgroundImage}
+              internalImage={item.nonProfit.logo}
+              mainText={currentTabData().currentItem(item).value}
+              secondaryText={currentTabData().currentItem(item).description}
+            />
+          </div>
+        ))}
       </S.ImpactContainer>
     </S.Container>
   );
