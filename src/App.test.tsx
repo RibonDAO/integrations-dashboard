ac2a1c789c7b9411a9dce490eb8d84253c4de766
Nicholas Marques <devnicknish@gmail.com>
2023-01-05 10:32:59 -0300

    fix: update jest react react-dom paths

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import { renderComponent, waitForPromises } from "config/testUtils";
+import { waitForPromises } from "config/testUtils";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 describe("renders learn react link", () => {
   it("renders without errors", async () => {
-    renderComponent(<App />);
+    render(<App />);
     await waitForPromises();
   });
 });
